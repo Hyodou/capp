5e0d50cab4354f2ecac3d05e7f858dd67f7eee15
Hyodou <victor160794@gmail.com>
2020-01-08 10:52:26 -0500

    CRUD Clientes

--- routes/routes.js
+++ routes/routes.js
@@ -16,5 +16,39 @@ const router = app => {
             response.send(result);
         });
     });
+
+    app.get('/clientes/:id', (request, response) =>{
+        pool.query('SELECT * FROM clientes WHERE idclientes = ?', request.params.id, (error, result) =>{
+            if(error) throw error;
+
+            response.send(result);
+        });
+    });
+
+    app.post('/clientes', (request, response)=>{
+        pool.query('INSERT INTO clientes SET ?', request.body, (error, result) =>{
+            if (error) throw error;
+
+            response.status(201).send('Usuario creado con CC: ${result.insertID}');
+        });
+    });
+
+    app.put('/clientes/:id', (request, response) =>{
+        const id = request.params.id;
+        pool.query('UPDATE clientes SET ? WHERE idclientes = ?', [request.body, id], (error, result) => {
+            if (error) throw error;
+    
+            response.send('User updated successfully.');
+        });
+    });
+
+    app.delete('/clientes/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('DELETE FROM clientes WHERE idclientes = ?', id, (error, result) => {
+            if (error) throw error;
+     
+            response.send('User deleted.');
+        });
+    });
 }
 module.exports = router;
